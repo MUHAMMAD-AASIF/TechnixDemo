f5dffb4da2e03427905801310f4bc5fe0b20c778
Muhammad Aasif Muhammad Amjad <muhammadaasif.muhammadamjad@msc.com>
2024-09-24 18:09:59 +0530

    bug Fixes

--- TechnixDemo/Service/FileGenerateService.cs
+++ TechnixDemo/Service/FileGenerateService.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
+﻿using System.Diagnostics;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using TechnixDemo.Model;
 using TechnixDemo.Templates;
 
--- TechnixDemo/Templates/DependencyInjection.cs
+++ TechnixDemo/Templates/DependencyInjection.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TechnixDemo.Model;
+﻿using System.Text;
 
 namespace TechnixDemo.Templates
 {
